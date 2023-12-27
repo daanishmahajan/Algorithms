ec1c1fd1decc4618931f0cfee9ac68f518836b9b
daanishmahajan <daanishmahajan27@gmail.com>
2023-12-27 09:05:39 +0530

    Update and rename Triple Repeats to repeats.cpp

--- repeats.cpp
+++ repeats.cpp
@@ -19,15 +19,14 @@ const int maxlogdepth = 30;
 
 struct node{
     int l, r, startIndex, depth; // depth is before starting index of this node
-    ll **colocated = new ll*[4];
+    ll *colocated = new ll[4];
     vector<node*> p;
     vector<node*> child;
     node(int left, int right, int start, int dist, node *parent):l(left), r(right), startIndex(start), depth(dist){
         p.resize(maxlogdepth);
         p[0] = parent;
         for(int i = 0; i < 4; i++){
-            colocated[i] = new ll[4];
-            for(int j = 0; j < 4; j++)colocated[i][j] = 0;
+            colocated[i] = 0;
         } 
     }
 };
@@ -97,6 +96,8 @@ public:
             if(start >= len)break;
             // cout << "start :" << start << " LCP: " << LCP << endl;
             if(LCP == H){ // haploid
+                // diploid
+                if(itr->startIndex >= 2 * H + 1 && SA[start] < 2 * H + 1)itr->startIndex = SA[start];
             }else if(LCP == 0){
                 node *child = new node(SA[start] + LCP, SA[start] + H - 1, SA[start], 0, root);
                 root->child.pb(child);
@@ -128,23 +129,9 @@ public:
     }
 };
 
-struct genome{
-    vector<string> h;
-    genome(){}
-    void Init(int number_of_haplotypes){h.resize(number_of_haplotypes);}
-};
-
 map<char, int> char_map = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
 
-pii baseBeforeStartIndex(SuffixTree *ST, int index){
-    index--;
-    assert(index >= ST->l0 - 1 && index <= ST->r0 - 1);
-    index = (index + ST->H) % ST->H;
-    char v1 = ST->text[index], v2 = ST->text[index + 2 * ST->H + 1];
-    return mp(char_map[min(v1, v2)], char_map[max(v1, v2)]);
-}
-
-ll** triplerepeat(SuffixTree *ST, node *u, map<int, ll> &m, bool *reduce, int depth){
+ll* triplerepeat(SuffixTree *ST, node *u, map<int, ll> &m, bool *reduce, int depth){
     // precomputation for finding the parent
     for(int i = 1; i < maxlogdepth; i++){
         if(u->p[i - 1] == ST->root)u->p[i] = ST->root;
@@ -152,16 +139,17 @@ ll** triplerepeat(SuffixTree *ST, node *u, map<int, ll> &m, bool *reduce, int de
         // cout << u->p[i]->l << " " << u->p[i]->r << endl;
     }
 
-    ll **freqU = new ll*[4]; // number of positions with min char on i and max char on j (unordered)
+    ll *freqU = new ll[4]; 
     for(int i = 0; i < 4; i++){
-        freqU[i] = new ll[4];
-        for(int j = 0; j < 4; j++)freqU[i][j] = 0;
+        freqU[i] = 0;
     } 
     if(u->child.empty()){// leaf node
         int baseindex = u->startIndex;
-        if(baseindex >= 2 * ST->H + 1)baseindex -= (2 * ST->H + 1);
-        pii base = baseBeforeStartIndex(ST, baseindex);
-        freqU[base.first][base.second] = 1;
+        if(baseindex >= 2 * ST->H + 1)baseindex -= 2 * ST->H + 1;
+        int beforeindex = (baseindex - 1 + ST->H) % ST->H;
+        if(u->startIndex >= 2 * ST->H + 1)beforeindex += 2 * ST->H + 1;
+        int base = char_map[ST->text[beforeindex]];
+        freqU[base] = 1;
         if(ST->closestloci[baseindex] != -1 && reduce[baseindex]){ // contributes to colocated repeats
             // cout << u->startIndex << endl;
             int searchdepth = (ST->closestloci[baseindex] - baseindex + ST->H) % ST->H;
@@ -172,88 +160,49 @@ ll** triplerepeat(SuffixTree *ST, node *u, map<int, ll> &m, bool *reduce, int de
                     ptr = ptr->p[i];
                 }
             }
-            ptr->colocated[base.first][base.second]++;
+            ptr->colocated[base]++;
         }
         if(!reduce[baseindex])reduce[baseindex] = true;
 
         return freqU;
     }
 
-    ll sumU[4][4] = {0}; // first character there second not there (order of haplotype doesn't matter)
-    ll sumnotU[4] = {0};  // character not there in both the haplotypes
+    ll tsumU = 0;  
     ll contribution = 0;
     for(node *v : u->child){
-        ll **freqV = triplerepeat(ST, v, m, reduce, depth + v->r - v->l + 1); // unordered
+        ll *freqV = triplerepeat(ST, v, m, reduce, depth + v->r - v->l + 1); // unordered
         
         // removing colocated double repeats
         for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                freqV[i][j] -= v->colocated[i][j];  
-            }
+            freqV[i] -= v->colocated[i];  
         }
 
-        cout << v->l << " " << v->r << endl;
-        for(int i = 0; i < 4; i++){
-            for(int j = i; j < 4; j++)cout << freqV[i][j] << " ";
-            cout << endl;
-        }
-        // for(int i = 0; i < 4; i++){
-        //     for(int j = i; j < 4; j++)cout << freqU[i][j] << " ";
-        //     cout << endl;
-        // }
+        // cout << v->l << " " << v->r << endl;
+        // for(int i = 0; i < 4; i++)cout << freqV[i] << " ";
+        // cout << endl;
+        // for(int i = 0; i < 4; i++)cout << freqU[i] << " ";
+        // cout << endl;
         
-        ll sumV[4][4] = {0}, sumnotV[4] = {0};
-        for(int i = 0; i < 4; i++){
-            for(int j = i; j < 4; j++){
-                for(int k = 0; k < 4; k++){
-                    if(k != i && k != j){
-                        sumV[i][k] += freqV[i][j];
-                        if(i != j)sumV[j][k] += freqV[i][j];
-
-                        sumnotV[k] += freqV[i][j];
-                    }
-                }
-                if(j > i){
-                    sumV[i][i] += freqV[i][j];
-                    sumV[j][j] += freqV[i][j];
-                }
-            }
-        }
+        ll tsumV = 0;
+        for(int i = 0; i < 4; i++)tsumV += freqV[i];
 
         // find contribution
         for(int i = 0; i < 4; i++){
-            for(int j = i; j < 4; j++){
-                if(j == i){
-                    // contribution of 1 from v
-                    contribution += ((sumU[i][i] + freqU[i][i]) * sumnotU[i] + (sumnotU[i] - 1) * sumnotU[i] / 2) * freqV[i][i];
-                    
-                    // contribution of 2 from v                    
-                    contribution += ((sumV[i][i] + freqV[i][i]) * sumnotV[i] + (sumnotV[i] - 1) * sumnotV[i] / 2) * freqU[i][i];
-                    continue;
-                }
-                // contribution of 1 from v
-                assert(sumnotU[i] - sumU[j][i] == sumnotU[j] - sumU[i][j]);
-                contribution += (sumU[i][j]  * sumnotU[i] + sumU[j][i] * sumnotU[j] - sumU[i][j] * sumU[j][i] + 
-                                freqU[i][j] * (sumnotU[i] - sumU[j][i]) + (sumnotU[i] - sumU[j][i] - 1) * (sumnotU[i] - sumU[j][i]) / 2) * freqV[i][j];
+            // cout << i << " " << j << " contribution = " << contribution << endl;
+            // contribution of 1 from v
+            contribution += (freqU[i] * (tsumU - freqU[i]) + (tsumU - freqU[i]) * (tsumU - freqU[i] - 1) / 2) * freqV[i];
 
-                // contribution of 2 from v
-                assert(sumnotV[i] - sumV[j][i] == sumnotV[j] - sumV[i][j]);
-                contribution += (sumV[i][j]  * sumnotV[i] + sumV[j][i] * sumnotV[j] - sumV[i][j] * sumV[j][i] + 
-                                freqV[i][j] * (sumnotV[i] - sumV[j][i]) + (sumnotV[i] - sumV[j][i] - 1) * (sumnotV[i] - sumV[j][i]) / 2) * freqU[i][j];
-            }
+            // contribution of 2 from v                    
+            contribution += (freqV[i] * (tsumV - freqV[i]) + (tsumV - freqV[i]) * (tsumV - freqV[i] - 1) / 2) * freqU[i];
         }
 
         // update freqU
         for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                freqU[i][j] += freqV[i][j];
-                sumU[i][j] += sumV[i][j];
-            }
-            sumnotU[i] += sumnotV[i];
+            freqU[i] += freqV[i];
+            tsumU += freqV[i];
         }
     }
-    cout << u->startIndex << " contribution = " << contribution << endl;
-
+    
     if(depth != 0 && contribution != 0){
         if(m.find(depth) == m.end())m[depth] = contribution;
         else m[depth] += contribution;
@@ -291,49 +240,42 @@ void triplerepeat_bruteDiploid(SuffixTree *ST, map<int, ll> &repeatstats_brute){
     for(int i = 0; i < l - 2; i++){
         for(int j = i + 1; j < l - 1; j++){
             for(int k = j + 1; k < l; k++){
-                for(int sl = l; sl >= 1; sl--){
-                    for(int g1 = 0; g1 < 2; g1++){
-                        char c1b[] = {s[(i - 1 + l) % l], s[(i - 1 + l) % l + 2 * l + 1]};
-                        char c1a[] = {s[i + sl], s[i + 2 * l + 1 + sl]}; 
+                bool found = false;
+                for(int sl = l; sl >= 1 && !found; sl--){
+                    for(int g1 = 0; g1 < 2 && !found; g1++){
+                        char c1l[] = {s[(i - 1 + l) % l], s[(i - 1 + l) % l + 2 * l + 1]};
+                        char c1r[] = {s[i + sl], s[i + 2 * l + 1 + sl]}; 
                         int s1 = i;
                         if(g1 == 1){
                             s1 += 2 * l + 1;
                             if(ST->closestloci[i] != -1 && (ST->closestloci[i] - i + l) % l >= sl)continue; // checking for colocated
                         }
-                        for(int g2 = 0; g2 < 2; g2++){
-                            char c2b[] = {s[j - 1], s[j + 2 * l]}; 
-                            char c2a[] = {s[j + sl], s[j + 2 * l + 1 + sl]};
+                        for(int g2 = 0; g2 < 2 && !found; g2++){
+                            char c2l[] = {s[j - 1], s[j + 2 * l]}; 
+                            char c2r[] = {s[j + sl], s[j + 2 * l + 1 + sl]};
                             int s2 = j;
                             if(g2 == 1){
                                 s2 += 2 * l + 1;
                                 if(ST->closestloci[j] != -1 && (ST->closestloci[j] - j + l) % l >= sl)continue; 
                             }
-                            for(int g3 = 0; g3 < 2; g3++){
-                                char c3b[] = {s[k - 1], s[k + 2 * l]}; 
-                                char c3a[] = {s[k + sl], s[k + 2 * l + 1 + sl]};
+                            for(int g3 = 0; g3 < 2 && !found; g3++){
+                                char c3l[] = {s[k - 1], s[k + 2 * l]}; 
+                                char c3r[] = {s[k + sl], s[k + 2 * l + 1 + sl]};
                                 int s3 = k;
                                 if(g3 == 1){
                                     s3 += 2 * l + 1;
                                     if(ST->closestloci[k] != -1 && (ST->closestloci[k] - k + l) % l >= sl)continue; 
                                 }
-                                bool maximal = true;
-                                for(int ptr1 = 0; ptr1 < 2 && maximal; ptr1++){
-                                    for(int ptr2 = 0; ptr2 < 2 && maximal; ptr2++){
-                                        for(int ptr3 = 0; ptr3 < 2 && maximal; ptr3++){
-                                            if(c1b[ptr1] == c2b[ptr2] && c2b[ptr2] == c3b[ptr3]){
-                                                maximal = false;
-                                            }
-                                            if(c1a[ptr1] == c2a[ptr2] && c2a[ptr2] == c3a[ptr3]){
-                                                maximal = false;
-                                            }
-                                        }
-                                    }
-                                }
-                                if(!maximal)continue;
+                                
+                                if(c1l[g1] == c2l[g2] && c2l[g2] == c3l[g3])continue;
+                                if(c1r[g1] == c2r[g2] && c2r[g2] == c3r[g3])continue;
+
                                 string st1 = s.substr(s1, sl), st2 = s.substr(s2, sl), st3 = s.substr(s3, sl);
                                 if(st1 == st2 && st2 == st3){
+                                    cout << sl << " " << i << " " << j << " " << k << " - " << g1 << " " << g2 << " " << g3 << endl;
                                     if(repeatstats_brute.find(sl) == repeatstats_brute.end())repeatstats_brute[sl] = 1;
                                     else repeatstats_brute[sl] = repeatstats_brute[sl] + 1;
+                                    found = true;
                                 }
                             }
                         }
@@ -354,22 +296,24 @@ void printSuffixTree(node *n){
 int main()
 {   
     while(true){
-        int l = 5;
+        // int l = 5, n = 2;
         // string s = "";
         // char c[] = {'A', 'C', 'G', 'T'};
         // for(int i = 0; i < l; i++){
         //     s += c[rnd.next(0, 3)];
         // }
-        
+        // set<int> locipst;
+        // while(locipst.size() < n){
+        //     locipst.insert(rnd.next(0, l - 1));
+        // }
         // string s1 = s; 
-        // int locipst = 2;
-        // s1[locipst] = c[(char_map[s[locipst]] + 1) % 4];
-        string s = "CCCTT", s1 = "TCTTT";
+        // for(set<int>::iterator it = locipst.begin(); it != locipst.end(); it++){
+        //     s1[*it] = c[(char_map[s[*it]] + 1) % 4];
+        // }
+
+        string s = "TGGGC", s1 = "TGTGG"; // code doesn't work for cases where one haplotype is the cyclic rotation of the other
         vector<string> v; v.pb(s); v.pb(s1); // diploid
-        // sort(v.begin(), v.end());
-        // cout << s << " " << s1 << endl;
         // vector<string> v; v.pb(s); v.pb(s); // haploid
-        // vector<string> v = {"CCCTT", "CCATT"};
         string query = v[0] + v[0] + '0' + v[1] + v[1] + '1';
         const char *text = query.c_str();
         // char text[] = "mississippi$";
@@ -381,10 +325,10 @@ int main()
 
         // SA
         divsufsort((unsigned char *)text, SA, len);
-        for(int i = 0; i < len; i++){
-            if((SA[i] >= 0 && SA[i] < v[0].length()) || (SA[i] >= 2 * v[0].length() + 1 && SA[i] <= 3 * v[0].length()))
-                cout << SA[i] << " \n"[i == len - 1];
-        }
+        // for(int i = 0; i < len; i++){
+        //     if((SA[i] >= 0 && SA[i] < v[0].length()) || (SA[i] >= 2 * v[0].length() + 1 && SA[i] <= 3 * v[0].length()))
+        //         cout << SA[i] << " ";
+        // }cout << endl;
 
         // iSA
         for(int i = 0; i < len; i++){
@@ -410,7 +354,7 @@ int main()
         // ST
         SuffixTree *ST = new SuffixTree(text, SA, lcp);
         free(SA);
-        printSuffixTree(ST->root);
+        // printSuffixTree(ST->root);
 
         map<int, ll> repeatstats; 
         bool *reduce = new bool[ST->H];
@@ -422,20 +366,20 @@ int main()
         map<int, ll> repeatstats_brute;
         // triplerepeat_bruteHaploid(ST, repeatstats_brute);
         triplerepeat_bruteDiploid(ST, repeatstats_brute);
-        
-        cout << "SuffixTree\n";
-        for(auto &stats : repeatstats){
-            cout << stats.first << " " << stats.second << endl;
-        }
-        cout << "Brute\n";
-        for(auto &stats : repeatstats_brute){
-            cout << stats.first << " " << stats.second << endl;
-        }
   
         if(repeatstats_brute != repeatstats){
             cout << v[0] << " " << v[1] << endl;
-            break;
-        }else cout << "OK\n";
+            cout << "SuffixTree\n";
+            for(auto &stats : repeatstats){
+                cout << stats.first << " " << stats.second << endl;
+            }
+            cout << "Brute\n";
+            for(auto &stats : repeatstats_brute){
+                cout << stats.first << " " << stats.second << endl;
+            }
+            // break;
+        }
+        // else cout << "OK\n";
 
         free(ST);
 
